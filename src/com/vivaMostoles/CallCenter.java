b195f1aace1a80ff3e16cdc11c2be2a68d464628
anton panizovich <anton_panizovich@tut.by>
2022-06-17 16:44:57 +0300

    work 16:44

--- src/com/vivaMostoles/CallCenter.java
+++ src/com/vivaMostoles/CallCenter.java
@@ -1,28 +1,26 @@
 package com.vivaMostoles;
-
 import java.util.concurrent.Semaphore;
-
 public class CallCenter {
+    private static int numberOfCallCenterManagers;
     //Парковочное место занято - true, свободно - false
-    private static final boolean[] CALLCENTER_NUMBER_OF_OPERATERS = new boolean[3];
+    private static final boolean[] CALLCENTER_NUMBER_OF_OPERATERS = new boolean[numberOfCallCenterManagers];
     //Устанавливаем флаг "справедливый", в таком случае метод
     //aсquire() будет раздавать разрешения в порядке очереди
-    private static final Semaphore SEMAPHORE = new Semaphore(10, true);
+    private static int numberOfCalls;
+    private static final Semaphore SEMAPHORE = new Semaphore(numberOfCalls, true);
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 25; i++) {
             new Thread(new Customer(i)).start();
-            Thread.sleep(400);
+            Thread.sleep(500);
         }
     }
-
     public static class Customer implements Runnable {
         private int customerNumber;
 
         public Customer(int customerNumber) {
             this.customerNumber = customerNumber;
         }
-
         @Override
         public void run() {
             System.out.printf("Клиент №%d позвонил в колл-центр.\n", customerNumber);
@@ -32,11 +30,11 @@ public class CallCenter {
                 //пока семафор не разрешит доступ
                 SEMAPHORE.acquire();
 
-                int operatorNumber = 1;
+                int operatorNumber = -1;
 
                 //Ищем свободное место и паркуемся
                 synchronized (CALLCENTER_NUMBER_OF_OPERATERS){
-                    for (int i = 0; i < 3; i++)
+                    for (int i = 1; i <= 5; i++)
                         if (!CALLCENTER_NUMBER_OF_OPERATERS[i]) {      //Если место свободно
                             CALLCENTER_NUMBER_OF_OPERATERS[i] = true;  //занимаем его
                             operatorNumber = i;         //Наличие свободного места, гарантирует семафор
@@ -45,12 +43,11 @@ public class CallCenter {
                         }
                 }
 
-                Thread.sleep(1000);       //Уходим за покупками, к примеру
+                Thread.sleep(3000);       //Уходим за покупками, к примеру
 
                 synchronized (CALLCENTER_NUMBER_OF_OPERATERS) {
                     CALLCENTER_NUMBER_OF_OPERATERS[operatorNumber] = false;//Освобождаем место
                 }
-
                 //release(), напротив, освобождает ресурс
                 SEMAPHORE.release();
                 System.out.printf("Клиент №%d закончил разговор.\n", customerNumber);
